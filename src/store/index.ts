687b53ac3587742660681d6e713eea184f69ebd3
Emmuui <nikityuk.off@gmail.com>
2022-09-07 18:37:12 +0300

    add thunk

--- src/store/index.ts
+++ src/store/index.ts
@@ -2,8 +2,12 @@ import { configureStore } from '@reduxjs/toolkit';
 import {userSlice} from "../features/auth/redux/user-slice";
 
 
+
 export const store = configureStore({
     reducer: {
         auth: userSlice.reducer,
     },
 });
+
+
+export const portalDispatch = store.dispatch;
