3f189b93bcd8a02091fa3c2f558300d698fdd1ec
Emmuui <nikityuk.off@gmail.com>
2022-09-09 11:46:56 +0300

    updated routes

---
+++ src/pages/not-found-page/index.tsx
@@ -0,0 +1,17 @@
+import styles from './styles.module.scss';
+import { Link } from 'react-router-dom';
+import React from 'react';
+
+
+const NotFound = () => {
+  return (
+    <div className={styles.not_found__div}>
+      <p className={styles.not_found__text}>
+        Oops. This page doesn`t exist. <Link to={'/'}>Go home</Link>
+      </p>
+    </div>
+  );
+}
+
+
+export default NotFound;
---
+++ src/pages/not-found-page/not-found-page.tsx
@@ -0,0 +1,12 @@
+import React, { ReactElement } from 'react';
+import { Link } from 'react-router-dom';
+
+const NotFoundPage = (): ReactElement => {
+  return (
+    <div>
+
+    </div>
+  );
+};
+
+export default NotFoundPage;
